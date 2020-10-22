cbea7fe74342f1d7ba82790494c03abec3ef2af5
NeuroWhAI <tlsehdgus0212@gmail.com>
2020-10-22 22:03:59 +0900

    Ready base code

---
+++ src/commands/basic.rs
@@ -0,0 +1,29 @@
+use crate::ShardManagerContainer;
+use serenity::prelude::*;
+use serenity::model::prelude::*;
+use serenity::framework::standard::{
+    CommandResult,
+    macros::command,
+};
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "Pong!").await?;
+
+    Ok(())
+}
+
+#[command]
+#[owners_only]
+async fn quit(ctx: &Context, msg: &Message) -> CommandResult {
+    let data = ctx.data.read().await;
+
+    if let Some(manager) = data.get::<ShardManagerContainer>() {
+        msg.reply(ctx, "Shutting down!").await?;
+        manager.lock().await.shutdown_all().await;
+    } else {
+        msg.reply(ctx, "There was a problem getting the shard manager").await?;
+    }
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,109 @@
+mod commands;
+
+use std::{
+    collections::HashSet,
+    env,
+    sync::Arc,
+};
+
+use serenity::{
+    async_trait,
+    client::bridge::gateway::ShardManager,
+    framework::{
+        StandardFramework,
+        standard::macros::group,
+    },
+    http::Http,
+    model::{event::ResumedEvent, gateway::Ready},
+    prelude::*,
+};
+
+use tracing::{error, info};
+use tracing_subscriber::{
+    FmtSubscriber,
+    EnvFilter,
+};
+
+use commands::basic::*;
+
+struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("Connected as {}", ready.user.name);
+    }
+
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
+}
+
+#[group]
+#[commands(ping, quit)]
+struct General;
+
+#[tokio::main]
+async fn main() {
+    // This will load the environment variables located at `./.env`.
+    dotenv::dotenv().expect("Failed to load .env file");
+
+    // Initialize the logger to use environment variables.
+    let subscriber = FmtSubscriber::builder()
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to start the logger");
+
+
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+
+    let http = Http::new_with_token(&token);
+
+    // Fetch bot's owners and id.
+    let (owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owner.id);
+
+            (owners, info.id)
+        },
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    // Create the framework.
+    let framework = StandardFramework::new()
+        .configure(|c| c
+            .owners(owners)
+            .prefix("!"))
+        .group(&GENERAL_GROUP);
+
+    let mut client = Client::new(&token)
+        .framework(framework)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Could not register ctrl+c handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
+    if let Err(why) = client.start().await {
+        error!("Client error: {:?}", why);
+    }
+}
