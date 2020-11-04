8d4f72d4319ddfcee5af46eba01c5965ba69707b
NeuroWhAI <tlsehdgus0212@gmail.com>
2020-11-04 20:30:48 +0900

    Add a model for stock

--- src/naver/model.rs
+++ src/naver/model.rs
@@ -58,6 +58,69 @@ pub struct Index {
     pub(crate) trading_value: i64,
 }
 
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Stock {
+    /// 이름.
+    #[serde(rename = "nm")]
+    pub(crate) name: String,
+
+    /// 장 상태.
+    #[serde(rename = "ms")]
+    pub(crate) state: MarketState,
+
+    /// 현재가(1원).
+    #[serde(rename = "nv")]
+    pub(crate) now_value: i64,
+
+    /// 장중최고(1원).
+    #[serde(rename = "hv")]
+    pub(crate) high_value: i64,
+
+    /// 장중최저(1원).
+    #[serde(rename = "lv")]
+    pub(crate) low_value: i64,
+
+    /// 등락종류(1 ~ 5 : 상한가, 상승, 보합, 하한가, 하락).
+    #[serde(rename = "rf")]
+    change_type: String,
+
+    /// 등락폭 절댓값(1원).
+    #[serde(rename = "cv")]
+    change_value: i64,
+
+    /// 등락률 절댓값(%).
+    #[serde(rename = "cr")]
+    change_rate: f64,
+
+    /// 거래량(1주).
+    #[serde(rename = "aq")]
+    pub(crate) trading_volume: i64,
+
+    /// 거래대금(1원).
+    #[serde(rename = "aa")]
+    pub(crate) trading_value: i64,
+}
+
+impl Stock {
+    /// 등락폭(1원).
+    pub fn change_value(&self) -> i64 {
+        if self.change_type == "4" || self.change_type == "5" {
+            -self.change_value
+        } else {
+            self.change_value
+        }
+    }
+
+    /// 등락률(%).
+    pub fn change_rate(&self) -> f64 {
+        if self.change_type == "4" || self.change_type == "5" {
+            -self.change_rate
+        } else {
+            self.change_rate
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -97,5 +160,28 @@ mod tests {
                 trading_value: 8941027,
             }
         );
+    }
+
+    #[test]
+    fn parse_stock() {
+        let data = r#" {"cd":"005930","nm":"삼성전자","sv":58800,"nv":58500,"cv":300,"cr":0.51,"rf":"5","mt":"1","ms":"CLOSE","tyn":"N","pcv":58800,"ov":58900,"hv":59000,"lv":57800,"ul":76400,"ll":41200,"aq":21316295,"aa":1245504000000,"nav":null,"keps":3166,"eps":3196,"bps":38533.50654,"cnsEps":4083,"dv":1416.00000} "#;
+        let stock: Stock = serde_json::from_str(data).unwrap();
+        assert_eq!(
+            stock,
+            Stock {
+                name: "삼성전자".into(),
+                state: MarketState::Close,
+                now_value: 58500,
+                high_value: 59000,
+                low_value: 57800,
+                change_type: "5".into(),
+                change_value: 300,
+                change_rate: 0.51,
+                trading_volume: 21316295,
+                trading_value: 1245504000000,
+            }
+        );
+        assert_eq!(stock.change_value(), -300);
+        assert_eq!(stock.change_rate(), -0.51);
     }
 }
